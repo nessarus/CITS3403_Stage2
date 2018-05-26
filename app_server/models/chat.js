5baade0215140d87f8dd2ee85b89f2ec7c5dc834
Qwazyoxyphi <39331035+Qwazyoxyphi@users.noreply.github.com>
2018-05-26 16:26:48 +0800

    Add files via upload

---
+++ app_server/models/chat.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+
+var msgSchema = new mongoose.Schema(
+    {   user:String,
+        message: String,
+        time: Date
+    });
+
+mongoose.model('Message',msgSchema, 'messages')
