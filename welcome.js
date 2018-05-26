4a9572ea7e2e2a5a64ddb159d0e47ee6a56a081d
Joshua <nessarus47@gmail.com>
2018-05-26 17:13:09 +0800

    welcome.js and headernav.css

---
+++ welcome.js
@@ -0,0 +1,24 @@
+/*date last modified*/
+function dater() {
+    var dateThis = "Last Modified Date: " + document.lastModified;
+    document.getElementById("last_date").innerHTML = dateThis;
+}
+
+//dynamic time
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('dynamic_time').innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+
+
